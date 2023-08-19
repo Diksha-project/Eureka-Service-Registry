b8bb25c1b6965c8bea8bac790a9be4322b6202ab
Diksha-project <diksha1998shukla@gmail.com>
2023-08-19 14:19:29 +0530

    Initial commit

---
+++ src/test/java/com/java/Eureka/demo/com/java/Eureka/demo/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.Eureka.demo.com.java.Eureka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
